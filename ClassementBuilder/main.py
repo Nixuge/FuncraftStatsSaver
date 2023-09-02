5c792b8fffe02a4f7f3b961a13a09747e91c85d4
Nixuge <mail@nixuge.me>
2023-09-02 13:06:40 +0200

    build_per_game changements

--- ClassementBuilder/main.py
+++ ClassementBuilder/main.py
@@ -1,3 +1,4 @@
+import gc
 import gzip
 import json
 import sqlite3
@@ -24,6 +25,7 @@ def build_per_game():
     cursor = connection.cursor()
 
     all_rows = cursor.execute("SELECT * FROM funcraft_stats;").fetchall()
+    # all_rows = cursor.execute("SELECT * FROM funcraft_stats;").fetchmany(5000)
 
     print("starting")
     start_time_total = time.time_ns()
@@ -50,7 +52,7 @@ def build_per_game():
                 currentStats = currentStats["stats"]
                 for stat in currentStats:
                     if stat["name"] == "glory":
-                        final_dict[game][month].append((row[0], row[1], row[1].upper(), float(stat["value"])))
+                        final_dict[game][month].append((row[0], float(stat["value"])))
                         break
 
 
@@ -63,40 +65,63 @@ def build_per_game():
         for month in final_dict[game]:
             month_db = month.replace("month-", "").replace("-", "_")
             newdb.cursor().execute(get_query_create(game, month_db))
-            final_dict[game][month].sort(key=take_fourth)
+            final_dict[game][month].sort(key=take_second)
             final_dict[game][month].reverse()
         newdb.commit()
 
     for game in GAMES:
         for month in final_dict[game]:
             for index, player in enumerate(final_dict[game][month]):
-                final_dict[game][month][index] = (*player, index + 1)
+                final_dict[game][month][index] = (player[0], index + 1)
     
     print(f"done in {(time.time_ns() - start_time) / 1000000000}s")
     start_time = time.time_ns()
     print("Starting final insert")
 
     _cursor = newdb.cursor()
-    count = 1
-    print("done done 1")
     for game in GAMES:
         for month in final_dict[game]:
             month_db = month.replace("month-", "").replace("-", "_")
-            query = f"INSERT INTO {game}__{month_db} VALUES (?, ?, ?, ?, ?)"
+            query = f"INSERT INTO {game}__{month_db} VALUES (?, ?)"
             
-            # print(query)
             _cursor.executemany(query, final_dict[game][month])
-            # for player in final_dict[game][month]:
-                # print(f"{player}")
-                # input()
-                # _cursor.execute(query, (player[0], player[1], player[2], player[4], player[3]))
-                # count += 1
-                # if count % 20000 == 0:
-                #     newdb.commit()
             newdb.commit()
     newdb.commit()
     newdb.close()
 
+    print(f"done in {(time.time_ns() - start_time) / 1000000000}s")
+    start_time = time.time_ns()
+    print("Starting building player specific dict")
+    player_dict: dict[str, dict[str, dict[str, dict]]] = {}
+    for game in final_dict:
+        for month in final_dict[game]:
+            for player in final_dict[game][month]:
+                id = player[0]
+                rank = player[1]
+                if not id in player_dict.keys():
+                    player_dict[id] = {}
+                if not game in player_dict[id].keys():
+                    player_dict[id][game] = {}
+                player_dict[id][game][month] = rank
+    print(f"done in {(time.time_ns() - start_time) / 1000000000}s")
+    print(f"cleaned up {gc.collect()} elements")
+
+    start_time = time.time_ns()
+    print("Compressing player rankings dicts")
+    insert_data = []
+    for playerid, data in player_dict.items():
+        insert_data.append((gzip.compress(bytes(json.dumps(data), 'utf-8')), playerid))
+        # final_dict.pop(playerid) # cleanup memory
+    print(f"done in {(time.time_ns() - start_time) / 1000000000}s")
+    
+    print(f"cleaned up {gc.collect()} elements")
+    start_time = time.time_ns()
+    print("Starting player specific dict insertion")
+
+    query = f"UPDATE funcraft_stats SET gzipped_rankings = (?) WHERE id == (?)"
+    newconnagain = sqlite3.connect("funcraft_database_website.db")
+    newconnagain.cursor().executemany(query, insert_data)
+    newconnagain.commit()
     print(f"done in {(time.time_ns() - start_time) / 1000000000}s")
     print(f"all done in {(time.time_ns() - start_time_total) / 1000000000}s")
     
@@ -104,8 +129,8 @@ def build_per_game():
         # json.dump(final_dict, file, indent=4)
 
 
-def take_fourth(elem):
-    return elem[3]
+def take_second(elem):
+    return elem[1]
 
 
 def remove_empty_tables():
@@ -129,12 +154,13 @@ def build_indexes():
     all_tables = connection.cursor().execute(sql_query).fetchall()
     for table in all_tables:
         print("building for " + table[0])
-        query1 = f"CREATE INDEX idx_{table[0]}_usernameup on {table[0]}(username_upper);"
-        query2 = f"CREATE INDEX idx_{table[0]}_ranking on {table[0]}(ranking);"
+        query1 = f"CREATE INDEX idx_{table[0]}_ranking on {table[0]}(ranking);"
         connection.cursor().execute(query1)
-        connection.cursor().execute(query2)
         connection.commit()
-        
-# build_per_game()
-# remove_empty_tables()
-# build_indexes()
+
+print("building")     
+build_per_game()
+print("removing empty tables")
+remove_empty_tables()
+print("building indexes")
+build_indexes()
--- ClassementBuilder/queries.py
+++ ClassementBuilder/queries.py
@@ -1,10 +1,6 @@
 def get_query_create(table_name: str, month: str):
         return f"""CREATE TABLE IF NOT EXISTS {table_name}__{month} (
                 id INT,
-                username VARCHAR(256),
-                username_upper VARCHAR(256),
-
-                points REAL,
                 ranking INT,
         
                 PRIMARY KEY (id)
